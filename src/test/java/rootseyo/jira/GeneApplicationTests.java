6b5b11e22b5ad2fa44bf6e3d44fb6ed8831c2914
seiyon.kim <root.seyo@gmail.com>
2020-01-02 16:07:52 +0900

    Init

---
+++ src/test/java/rootseyo/jira/GeneApplicationTests.java
@@ -0,0 +1,13 @@
+package rootseyo.jira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
